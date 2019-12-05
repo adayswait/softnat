557b632dd64c4bdd83fb30d2c4efb79c052b45f7
adayswait <adayswait@139.com>
2019-12-05 16:58:15 +0800

    c2c impl

--- softnat.cc
+++ softnat.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include "softnat.h"
 
 using tcp_t = asio::ip::tcp;
@@ -7,17 +8,17 @@ using addr_t = asio::ip::address_v4;
 void l2c(const char *ip, unsigned short port,
          const char *dest_ip, unsigned short dest_port)
 {
-    asio::io_context io_context;
+    asio::io_context io_context(std::thread::hardware_concurrency());
     tcp_t::endpoint localhost(addr_t().from_string(ip), port);
     tcp_t::endpoint remote(addr_t().from_string(dest_ip), dest_port);
     tcp_t::acceptor acceptor(io_context, std::move(localhost));
-    std::function<void()> ac =[&]() -> void {
+    std::function<void()> ac = [&]() -> void {
         acceptor.async_accept([&](std::error_code ec, tcp_t::socket sock) {
             if (!ec)
             {
                 auto oppo = tcp_t::socket(io_context);
                 std::make_shared<twine>(std::move(sock), std::move(oppo))
-                    ->dial_oppo(remote);
+                    ->estab(localhost, remote);
             }
             ac();
         });
@@ -26,9 +27,20 @@ void l2c(const char *ip, unsigned short port,
     io_context.run();
 }
 
+void c2c(const char *insider_ip, unsigned short insider_port,
+         const char *handler_ip, unsigned short handler_port)
+{
+    asio::io_context io_context(std::thread::hardware_concurrency());
+    tcp_t::endpoint insider(addr_t().from_string(insider_ip), insider_port);
+    tcp_t::endpoint handler(addr_t().from_string(handler_ip), handler_port);
+    undercover uc(&io_context, insider, handler);
+    uc.estab();
+    io_context.run();
+}
+
 int main(int argc, char *argv[])
 {
     // l2c("10.1.1.248", 3001, "10.1.8.135", 3000);
-    l2c("10.1.1.248", 3001, "10.1.1.248", 6379);
+    c2c("10.1.8.135", 3002, "10.1.8.135", 3001);
     return 0;
 }
--- softnat.h
+++ softnat.h
@@ -11,56 +11,85 @@ public:
         : _sock(std::move(sock)), _oppo(std::move(oppo))
     {
     }
-    void translate()
+    twine(const twine &) = delete;
+    twine &operator=(const twine &) = delete;
+    void estab(asio::ip::tcp::endpoint sock_remote,
+               asio::ip::tcp::endpoint oppo_remote)
     {
+        estab_sock(sock_remote);
+        estab_oppo(oppo_remote);
+    }
+    void translate() {
         recv_sock();
         recv_oppo();
     }
-    void send_oppo(std::size_t recv_n)
-    {
+    void send_oppo(std::size_t recv_n) {
         auto self(shared_from_this());
         asio::async_write(_oppo, asio::buffer(_sockbuf, recv_n),
             [this, self](const asio::error_code &ec, std::size_t recv_n) {
-                if(ec) return;
+                if (ec)
+                    return;
                 recv_sock();
         });
     }
-    void send_sock(std::size_t recv_n)
-    {
+    void send_sock(std::size_t recv_n) {
         auto self(shared_from_this());
-        asio::async_write(_sock, asio::buffer(_oppobuf, recv_n), 
+        asio::async_write(_sock, asio::buffer(_oppobuf, recv_n),
             [this, self](const asio::error_code &ec, std::size_t recv_n) {
-                if(ec) return;
+                if (ec)
+                    return;
                 recv_oppo();
         });
     }
-    void recv_oppo()
-    {
+    void recv_oppo() {
         auto self(shared_from_this());
         _oppo.async_read_some(asio::buffer(_oppobuf, MAX_LEN),
             [this, self](const asio::error_code &ec, std::size_t recv_n) {
-                if(ec) return;
+                if (ec)
+                    return;
                 send_sock(recv_n);
         });
-    };
-    void recv_sock()
-    {
+    }
+    void recv_sock() {
         auto self(shared_from_this());
         _sock.async_read_some(asio::buffer(_sockbuf, MAX_LEN),
             [this, self](const asio::error_code &ec, std::size_t recv_n) {
-                if(ec) return;
+                if (ec)
+                    return;
                 send_oppo(recv_n);
         });
-    };
-    void dial_oppo(asio::ip::tcp::endpoint remote)
-    {
-        auto self(shared_from_this());
-        _oppo.async_connect(remote,
-            [this, self](const asio::error_code &ec) {
-                if(ec) return;
-                asio::ip::tcp::no_delay option(true);
+    }
+    void estab_oppo(asio::ip::tcp::endpoint remote) {
+        if (_oppo.is_open()) {
+            if (_sock.is_open()) {
                 translate();
-        });
+            }
+        } else {
+            auto self(shared_from_this());
+            _oppo.async_connect(remote,
+                [this, self](const asio::error_code &ec) {
+                    if (ec) return;
+                    if (_sock.is_open()) {
+                        translate();
+                    }
+            });
+        }
+    }
+    void estab_sock(asio::ip::tcp::endpoint remote) {
+        if (_sock.is_open()) {
+            if (_oppo.is_open()) {
+                translate();
+            }
+        } else {
+            auto self(shared_from_this());
+            _sock.async_connect(remote,
+                [this, self](const asio::error_code &ec) {
+                    if (ec) return;
+                    if (_oppo.is_open()){
+                        translate();
+                    }
+            });
+        }
     }
 
 private:
@@ -68,4 +97,56 @@ private:
     asio::ip::tcp::socket _oppo;
     char _sockbuf[MAX_LEN];
     char _oppobuf[MAX_LEN];
+};
+
+class undercover
+{
+public:
+    undercover(asio::io_context *io_context,
+               asio::ip::tcp::endpoint &insider,
+               asio::ip::tcp::endpoint &handler)
+        : _p_ioc(io_context),
+          _ordersock(*io_context),
+          _insider(insider),
+          _handler(handler)
+    {
+    }
+
+    void estab()
+    {
+        _ordersock.async_connect(_handler, 
+            [this](const asio::error_code &ec) {
+                if (ec)return;
+                recv_order();
+        });
+    }
+
+    void recv_order()
+    {
+        _ordersock.async_read_some(asio::buffer(_orders, MAX_LEN),
+            [this](const asio::error_code &ec, std::size_t recv_n) {
+                if (ec) return;
+                for (size_t i = 0; i < recv_n; i++) {
+                    create_twine();
+                }
+                recv_order();
+        });
+    }
+    void create_twine()
+    {
+        std::make_shared<twine>(asio::ip::tcp::socket(*_p_ioc),
+                                asio::ip::tcp::socket(*_p_ioc))
+            ->estab(_handler, _insider);
+    }
+
+private:
+    char _orders[MAX_LEN];
+    asio::io_context *_p_ioc;
+    asio::ip::tcp::socket _ordersock;
+    asio::ip::tcp::endpoint &_handler;
+    asio::ip::tcp::endpoint &_insider;
+};
+
+class handler
+{
 };
